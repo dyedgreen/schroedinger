16f132d2a487cf09ec7de2a82c934782e3ed622c
Tilman Roeder <dyed.green.info@gmail.com>
2018-05-29 11:33:48 +0100

    Added a few interesting potentials.

--- schroedinger.py
+++ schroedinger.py
@@ -6,68 +6,78 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from numerov import eigenvalues, units
-import numerov_c as num_c
+import numerov_c as num
 
+# General settings
+l = 2e-9
 
-# Find the energies for the empty well
-l = 20e-10
-l_print = l
-l_inf = 4e-10
-mass = units.me
-s_c = 1000
+# Potential functions
+pot = []
+mass = []
+n = []
+names = []
 
-initial = 0.1
+# 14eV deep, 4nm wide square well
+def potSquareWell(x):
+    if x > -5e-10 and x < -1e-10:
+        return 0.
+    return 14. * units.e
+pot.append(potSquareWell)
+mass.append(units.me)
+n.append(3)
+names.append("Electron in 14eV Square Well")
 
-def pot(x):
-  if x > -5e-10 and x < -1e-10:
-    return 0.0
-  return 14.0 * units.e
-  # if (np.abs(x * 1e10) >= units.pi):
-  #   return 2 * 14. * 1.6e-19
-  # return -np.cos(x * 1e10) * 14. * 1.6e-19 + 14. * 1.6e-19
+# Slanted potential well
+def potQHM(x):
+    return 14. * units.e * units.e * (x/2e-10)**2
+pot.append(potQHM)
+mass.append(units.me)
+n.append(3)
+names.append("Electron in Quantum Harmonic Oscillator")
 
+# Slanted potential well
+def potSlantedWell(x):
+    if x > -5e-10 and x < -1e-10:
+        return 14. * units.e * (x - 5e-10) / 4e-10
+    return 14. * units.e
+pot.append(potSlantedWell)
+mass.append(units.me)
+n.append(1)
+names.append("Electron in Slanted 14eV Well")
 
-energies = num_c.energy(pot, mass, -l, l, initial, initial, s_c, 3)
-print(energies)
+########################################
+# Solve, plot and print all potentials #
+########################################
 
-#correction = (units.h**2 / 8 / mass / (l_inf)**2) / units.unscaleE(energies[0], mass)
-#print(correction)
+def underline(length = 0):
+    line = ""
+    for i in range(length):
+        line += "="
+    return line
 
-x_pot = np.linspace(-l, l_print, s_c)
-y_pot = np.empty(len(x_pot))
-for i in range(len(x_pot)):
-  y_pot[i] = pot(x_pot[i])
-y_pot *= .5 / np.max(y_pot)
-plt.plot(x_pot, y_pot, 'k-')
-
-i = -1
-col = ['r:','g:','b:','c:', 'm:', 'y:']
-for en in energies:
-  i += 1
-  # Make graph pretty
-  # if i == 2:
-  #   continue
-
-  x_val = np.linspace(-l_print, l_print, s_c)
-  y_val = eigenvalues.psi(pot, mass, en, -l_print, l_print, initial, initial, s_c)
-  #y_val_c = eigenvalues.psi(pot, mass, energies_c[i], -l_print, l_print, initial, initial, s_c)
-  plt.plot(x_val, y_val, col[(i) % len(col)], label=str(i))
-  #plt.plot(x_val, y_val_c, col_c[(i) % len(col_c)], label="c " + str(i))
-
-  print(
-    "[" +str(i)+ "]",
-    units.unscaleE(energies[i], mass) / units.e,
-    (i+1)**2 * units.h**2 / 8 / mass / (l_inf)**2,
-    units.unscaleE(energies[i], mass) - (i+1)**2 * units.h**2 / 8 / mass / (l_inf)**2
-  )
-  #print(y_val)
-
-# # x_val = np.linspace(-l, l, s_c)
-# # y_val = eigenvalues.psi(pot, mass, 0.73294830903, -l, l, 0, 0, s_c)
-# # plt.plot(x_val, y_val, 'r:', label="Test")
-
-# #plt.ylim((-4,4))
-
-plt.grid(True)
-plt.legend()
-plt.savefig('results/test_energies.png')
+# Print all the graphs
+x = np.linspace(-l, l, 1000)
+colors = ['r:','g:','b:','c:', 'm:', 'y:']
+for i in range(len(pot)):
+    # Find energies an print values
+    energies = num.energy(pot[i], mass[i], -l, l, 0.1, 0.1, 1000, n[i])
+    print("\nResults for " + names[i])
+    print(underline(12+len(names[i])))
+    print()
+    # Graph potential
+    pot_y = np.empty(len(x))
+    for j in range(len(x)):
+        pot_y[j] = pot[i](x[j])
+    pot_y *= .5 / np.max(pot_y)
+    plt.plot(x, pot_y, 'k-')
+    # Graph wave functions
+    for j in range(len(energies)):
+        print("["+str(j)+"] " + str(units.unscaleE(energies[j], mass[i]) / units.e) + " eV")
+        psi = eigenvalues.psi(pot[i], mass[i], energies[j], -l, l, 0.1, 0.1, 1000)
+        plt.plot(x, psi, colors[(j) % len(colors)], label=str(units.unscaleE(energies[j], mass[i]) / units.e)[:5] + " eV")
+    # Save plot
+    plt.grid(True)
+    plt.legend()
+    plt.xlabel("meters")
+    plt.savefig("results/"+"_".join(names[i].lower().split()))
+    plt.clf()
